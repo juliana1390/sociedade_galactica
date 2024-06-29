26a567d4d6c8bbb4f0f25e82c560d07725d5720c
juliana1390 <juliana13290@gmail.com>
2024-06-29 00:37:58 -0300

    app

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -19,8 +19,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from app.views import index
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    # app:index
+    path('', index, name='index'),
 ]
 
 if settings.DEBUG:
