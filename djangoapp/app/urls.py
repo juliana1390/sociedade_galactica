996dfbb1d6d05261721c5fd115efd853fda4184c
juliana1390 <juliana13290@gmail.com>
2024-06-30 19:23:04 -0300

    login page

--- djangoapp/app/urls.py
+++ djangoapp/app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from app.views import index
-
+from app.views import *
+from . import views
 
 app_name = 'app'
 
 urlpatterns = [
     # app:index
     path('', index, name='index'),
+    path('login_page/', login, name='login'),
+    path('login_check/', login_check, name='login_check'),
 ]
--- djangoapp/app/views.py
+++ djangoapp/app/views.py
@@ -1,8 +1,64 @@
 from django.shortcuts import render
-
+from django.views.decorators.http import require_POST
+from app.models import *
 
 def index(request):
     return render(
         request,
         'app/index.html'
     )
+
+
+def login(request):
+    return render(
+        request,
+        'app/login_page.html'
+    )
+
+@require_POST
+def login_check(request):
+    cpi = request.POST.get('cpi')
+    password = request.POST.get('password')
+    
+    try:
+        # Buscar usuário pelo cpi (id_lider)
+        user = Users.objects.get(id_lider=cpi)
+        
+        if user.password == password:
+            lider = user.id_lider
+            
+            # Armazenar o cpi na sessão
+            request.session['cpi'] = cpi
+            request.session['user_id'] = user.user_id
+            
+            # Verificar se o líder é também líder de facção
+            is_lider_faccao = Faccao.objects.filter(lider=lider).exists()
+            
+            # Redirecionar com base no cargo e se é líder de facção
+            cargo = lider.cargo.strip()
+            
+            if cargo == 'COMANDANTE':
+                if is_lider_faccao:
+                    return render(request, 'app/comandante_lider_faccao.html')
+                else:
+                    return render(request, 'app/comandante.html')
+            elif cargo == 'OFICIAL':
+                if is_lider_faccao:
+                    return render(request, 'app/oficial_lider_faccao.html')
+                else:
+                    return render(request, 'app/oficial.html')
+            elif cargo == 'CIENTISTA':
+                if is_lider_faccao:
+                    return render(request, 'app/cientista_lider_faccao.html')
+                else:
+                    return render(request, 'app/cientista.html')
+            else:
+                mensagem = "Erro ao fazer login: Página não encontrada."
+                return render(request, 'app/error_page.html', {'mensagem': mensagem})
+        else:
+            mensagem = "Erro ao fazer login: Senha incorreta."
+            return render(request, 'app/error_page.html', {'mensagem': mensagem})
+    
+    except Users.DoesNotExist:
+        mensagem = "Erro ao fazer login: Usuário não encontrado."
+        return render(request, 'app/error_page.html', {'mensagem': mensagem})
